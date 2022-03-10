20198e61768e3cc5abc50a70cc125658296ac810
Prince Bansal <prince.bansal008@gmail.com>
2022-03-11 04:42:30 +0530

    updated

--- [Ongoing Live Placement Course]/9.Pointers/1.BasicsPointer.cpp
+++ [Ongoing Live Placement Course]/9.Pointers/1.BasicsPointer.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 using namespace std;
-#define nl "\n" 
+#define nl "\n"
 
-int main(){
+int main() {
     int n = 5;
     int *ptr = &n;
-    cout<<"value of n:"<<n<<nl;
-    cout<<"value of n:"<<*ptr<<nl;
-    cout<<"Address of n:"<<&n<<nl;
-    cout<<"Address of n:"<<ptr<<nl;
+    cout << "value of n: " << n << nl;
+    cout << "value of n: " << *ptr << nl;
+    cout << "Address of n: " << &n << nl;
+    cout << "Address of n: " << ptr << nl;
+    cout << "size of int var: " << sizeof(n) << nl;
+    cout << "size of pointer var: " << sizeof(ptr) << nl;
+    
+    int *temp = NULL;
+    // cout << *temp<< nl; // error
     return 0;
 }
