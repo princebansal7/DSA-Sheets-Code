8b22573ae7b3a44708b09d5abb888f66109f45b7
Prince Bansal <prince.bansal008@gmail.com>
2022-03-11 04:18:31 +0530

    pointer file added

---
+++ [Ongoing Live Placement Course]/9.Pointers/1.BasicsPointer.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#define nl "\n" 
+
+int main(){
+    int n = 5;
+    int *ptr = &n;
+    cout<<"value of n:"<<n<<nl;
+    cout<<"value of n:"<<*ptr<<nl;
+    cout<<"Address of n:"<<&n<<nl;
+    cout<<"Address of n:"<<ptr<<nl;
+    return 0;
+}
