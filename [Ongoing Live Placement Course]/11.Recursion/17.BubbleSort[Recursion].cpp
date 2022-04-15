0e4c332c3fdc3c86f3b3d7ad82717073343dcde5
Prince Bansal <prince.bansal008@gmail.com>
2022-04-16 01:19:12 +0530

    Sorting with recursion

---
+++ [Ongoing Live Placement Course]/11.Recursion/17.BubbleSort[Recursion].cpp
@@ -0,0 +1,45 @@
+/*
+    @author: princebansal_
+    Types of Sorting algo [GFG]: https://www.geeksforgeeks.org/classification-of-sorting-algorithms/
+*/
+
+#include <iostream>
+#include <utility>
+using namespace std;
+#define nl "\n"
+#define ll long long
+
+int arr[100];
+
+void bubbleSort(int arr[], int n)
+{
+    // base case
+    if (n == 0 or n == 1)
+        return;
+
+    // solve 1 case: put largest element to end
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+            swap(arr[i], arr[i + 1]);
+    }
+
+    // Recursive call for remaining part
+    bubbleSort(arr, n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    bubbleSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << nl;
+
+    return 0;
+}
---
+++ [Ongoing Live Placement Course]/11.Recursion/19.InsertionSort[Recursion].cpp
@@ -0,0 +1,40 @@
+/*
+    @author: princebansal_
+    Types of Sorting algo [GFG]: https://www.geeksforgeeks.org/classification-of-sorting-algorithms/
+*/
+
+#include <iostream>
+#include <utility>
+using namespace std;
+#define nl "\n"
+#define ll long long
+
+int arr[100];
+
+void insertionSort(int arr[], int n)
+{
+    // base case
+    if (n == 0 or n == 1)
+        return;
+
+    // solve 1 case: put largest element to end
+
+    // Recursive call for remaining part
+    insertionSort(arr, n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    insertionSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << nl;
+
+    return 0;
+}
