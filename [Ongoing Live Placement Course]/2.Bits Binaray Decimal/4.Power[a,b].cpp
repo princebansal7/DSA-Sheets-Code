fb1f5743812d14e8c1edff714d6e3102a035bef1
Prince Bansal <prince.bansal008@gmail.com>
2022-04-15 13:32:21 +0530

    updated

--- [Ongoing Live Placement Course]/2.Bits Binaray Decimal/4.Power[a,b].cpp
+++ [Ongoing Live Placement Course]/2.Bits Binaray Decimal/4.Power[a,b].cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main()
 {
 
-#ifndef ONLINE_JUDGE
-    freopen("debug.txt", "w", stderr);
-#endif
+    // #ifndef ONLINE_JUDGE
+    //     freopen("debug.txt", "w", stderr);
+    // #endif
 
     int a, b;
     cin >> a >> b;
     long long ans = 1;
-    cout << "Value of " << a << "^" << b << " is: ";
+    cout << a << "^" << b << " is: ";
     for (int i = 1; i <= b; i++)
     {
         ans = ans * a;
