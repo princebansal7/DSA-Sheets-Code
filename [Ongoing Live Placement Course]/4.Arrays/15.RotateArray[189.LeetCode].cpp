c814295ed133e08aaf033cf67053fe1108f1bc76
Prince Bansal <prince.bansal008@gmail.com>
2022-05-07 14:18:46 +0530

    updated

--- [Ongoing Live Placement Course]/4.Arrays/15.RotateArray[189.LeetCode].cpp
+++ [Ongoing Live Placement Course]/4.Arrays/15.RotateArray[189.LeetCode].cpp
@@ -1,20 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void rotate(vector<int>& a, int k) {
+void rotate(vector<int> &a, int k)
+{
 
-    // Clockwise rotate
+    // Clockwise rotate => right rotation
 
     int n = a.size();
     vector<int> temp(n);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
 
         temp[(i + k) % n] = a[i];
     }
     a = temp;
 }
-
 
 int main()
 {
@@ -23,17 +24,17 @@ int main()
     freopen("debug.txt", "w", stderr);
 #endif
 
-    int n, k; cin >> n >> k;
+    int n, k;
+    cin >> n >> k;
     vector<int> a(n);
 
-    for (int i = 0 ; i < n ; i++)
+    for (int i = 0; i < n; i++)
         cin >> a[i];
 
     rotate(a, k);
 
-    for (int i = 0 ; i < n ; i++)
+    for (int i = 0; i < n; i++)
         cout << a[i] << " ";
-
 
     return 0;
 }
