9cc8cb7d7c6a677b148f581dd35d32ee44ae256f
Prince Bansal <prince.bansal008@gmail.com>
2023-10-02 01:50:59 +0530

    test file updated

--- test.cpp
+++ test.cpp
@@ -5,17 +5,5 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    int cnt = 0;
-    int temp = n;
-    while (n <= 0) {
-        int digit = n % 10;
-        if (digit == 0)
-            continue;
-        if (temp % digit == 0)
-            cnt++;
-        n = n / 10;
-    }
-    cout << cnt << nl;
+    // testing code
 }
