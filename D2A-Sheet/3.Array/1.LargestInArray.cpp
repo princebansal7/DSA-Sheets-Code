1385417929c8591e8b282fcdd92fb4e7f02a2712
Prince Bansal <prince.bansal008@gmail.com>
2024-04-26 03:19:41 +0530

    updated code

--- D2A-Sheet/3.Array/1.LargestInArray.cpp
+++ D2A-Sheet/3.Array/1.LargestInArray.cpp
@@ -9,15 +9,18 @@
 using namespace std;
 #define nl '\n'
 
+// works only when no duplicates
+
 int bruteForceApproach(vector<int>& v, int n)
 {
     sort(v.begin(), v.end());
     return v[n - 1];
 }
+
 int recursiveApproach(vector<int>& v, int n)
 {
     int ans = v[0];
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
         if (v[i] > ans)
             ans = v[i];
     return ans;
@@ -25,7 +28,7 @@ int recursiveApproach(vector<int>& v, int n)
 
 int main()
 {
-    vector<int> v { 1, 2, 3, 4, 44, 3, 232, 12, 1, 23, 4, 5, 6 };
+    vector<int> v { 1, 2, 3, 4, 44, 69, 12, 23, 5, 6 };
     int n = v.size();
     cout << "Max value is: " << bruteForceApproach(v, n) << nl;
     cout << "Max value is: " << recursiveApproach(v, n) << nl;
