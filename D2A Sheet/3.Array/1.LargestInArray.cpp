16f96c8cdc96106de2cad1b35c3bff38f7c90f9e
Prince Bansal <prince.bansal008@gmail.com>
2024-04-26 01:41:53 +0530

    added both methods

--- D2A Sheet/3.Array/1.LargestInArray.cpp
+++ D2A Sheet/3.Array/1.LargestInArray.cpp
@@ -1,20 +1,33 @@
 /*
     @author: princebansal_
+    Sheet: https://takeuforward.org/strivers-a2z-dsa-course/strivers-a2z-dsa-course-sheet-2/
 */
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 #define nl '\n'
 
-int main()
+int bruteForceApproach(vector<int>& v, int n)
 {
-    vector<int> v { 1, 2, 3, 4, 44, 3, 232, 12, 1, 23, 4, 5, 6 };
-    int n = v.size();
+    sort(v.begin(), v.end());
+    return v[n - 1];
+}
+int recursiveApproach(vector<int>& v, int n)
+{
     int ans = v[0];
     for (int i = 0; i < n; i++)
         if (v[i] > ans)
             ans = v[i];
-    cout << "max value is: " << ans << nl;
+    return ans;
+}
+
+int main()
+{
+    vector<int> v { 1, 2, 3, 4, 44, 3, 232, 12, 1, 23, 4, 5, 6 };
+    int n = v.size();
+    cout << "Max value is: " << bruteForceApproach(v, n) << nl;
+    cout << "Max value is: " << recursiveApproach(v, n) << nl;
     return 0;
 }
