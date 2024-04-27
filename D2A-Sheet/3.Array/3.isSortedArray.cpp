f93c405b65dbfaff22c32039ee61fe78e7910000
Prince Bansal <prince.bansal008@gmail.com>
2024-04-28 01:13:54 +0530

    optimal approach added

--- D2A-Sheet/3.Array/3.isSortedArray.cpp
+++ D2A-Sheet/3.Array/3.isSortedArray.cpp
@@ -2,15 +2,11 @@
     @author: princebansal_
     Sheet: https://takeuforward.org/strivers-a2z-dsa-course/strivers-a2z-dsa-course-sheet-2/
 */
-#include <algorithm>
-#include <climits>
+
 #include <iostream>
-#include <utility>
 #include <vector>
 using namespace std;
 #define nl '\n'
-
-// works only when no duplicates
 
 bool bruteForceApproach(vector<int>& v, int n)
 {
@@ -23,22 +19,28 @@ bool bruteForceApproach(vector<int>& v, int n)
     return true;
 }
 
-// bool betterApproach(vector<int>& v, int n)
-// {
-// }
-
-// bool optimalApproach(vector<int>& v, int n)
-// {
-// }
+bool optimalApproach(vector<int>& v, int n)
+{
+    if (n == 1)
+        return true;
+    for (int i = 1; i < n; i++) {
+        int prev = v[i - 1];
+        int curr = v[i];
+        if (prev > curr)
+            return false;
+    }
+    return true;
+}
 
 int main()
 {
-    // vector<int> v { 7, 2, 1, 4, 5, 6, 7, 7, 7, 45, 69, 100, 12, 23, 5, 6 };
-    vector<int> v { 1, 2, 3, 4, 4, 5, 6, 7, 8, 8, 9, 10, 4 };
+    vector<int> v { 7, 2, 1, 4, 5, 6, 7, 7, 7, 45, 69, 100, 12, 23, 5, 6 };
+    // vector<int> v { 1, 2, 3, 4, 4, 5, 6, 7, 8, 8, 9, 10 };
     int n = v.size();
 
     bool isSorted;
-    isSorted = bruteForceApproach(v, n);
+    // isSorted = bruteForceApproach(v, n);
+    isSorted = optimalApproach(v, n);
 
     if (isSorted)
         cout << "Array is sorted" << nl;
