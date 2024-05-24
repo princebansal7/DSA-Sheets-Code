846bae75e6a384c14e89f2359329a947ddb6e550
Prince Bansal <prince.bansal008@gmail.com>
2024-05-25 03:42:17 +0530

    added cpp version check file

---
+++ versionCheck.cpp
@@ -0,0 +1,27 @@
+/*
+    @author: princebansal_
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define nl '\n'
+
+int main()
+{
+
+    if (__cplusplus == 202302L)
+        cout << "C++23\n";
+    else if (__cplusplus == 202002L)
+        cout << "C++20\n";
+    else if (__cplusplus == 201703L)
+        cout << "C++17\n";
+    else if (__cplusplus == 201402L)
+        cout << "C++14\n";
+    else if (__cplusplus == 201103L)
+        cout << "C++11\n";
+    else if (__cplusplus == 199711L)
+        cout << "C++98\n";
+    else
+        cout << "pre-standard C++\n";
+    return 0;
+}
