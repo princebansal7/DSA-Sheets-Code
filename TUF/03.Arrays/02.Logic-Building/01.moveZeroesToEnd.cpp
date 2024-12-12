8f3f47e7e8d494f5ebfaf6e4607a10cc2fe9890e
Prince Bansal <75045465+princebansal7@users.noreply.github.com>
2024-12-12 16:44:26 +0530

    move-zero to end

---
+++ TUF/03.Arrays/02.Logic-Building/01.moveZeroesToEnd.cpp
@@ -0,0 +1,71 @@
+/*
+    @author: princebansal_
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+#define nl '\n'
+// Given an integer array nums, move all the 0's to the end of the array.
+// The relative order of the other elements must remain the same
+// Input : nums = [ 0, 1, 4, 0, 5, 2 ]
+// Output : [ 1, 4, 5, 2, 0, 0 ]
+
+// TC: O(N) + O(N), SC: O(N)
+void moveZeroesToEndBute(vector<int>& nums)
+{
+    vector<int> temp;
+    int size = nums.size();
+    for (int i = 0; i < size; i++) {
+        if (nums[i] != 0)
+            temp.push_back(nums[i]);
+    }
+    int tempSize = temp.size();
+    for (int i = 0; i < tempSize; i++) {
+        nums[i] = temp[i];
+    }
+    for (int i = tempSize; i < size; i++) {
+        nums[i] = 0;
+    }
+}
+
+// TC: O(N), SC: O(N) - two pointer approach
+void moveZeroesToEndOptimal(vector<int>& nums)
+{
+    int zeroPosition = -1;
+    int size = nums.size();
+
+    // going to the first 0th position
+    for (int i = 0; i < size; i++) {
+        if (nums[i] == 0) {
+            zeroPosition = i;
+            break;
+        }
+    }
+    // if array does have any zeroes - return
+    if (zeroPosition == -1)
+        return;
+
+    // Here, swapping the non zero value with 0 till end
+    for (int i = zeroPosition + 1; i < size; i++) {
+        if (nums[i] != 0) {
+            swap(nums[zeroPosition], nums[i]);
+            zeroPosition++;
+        }
+    }
+}
+
+int main()
+{
+    vector<int> nums = { 0, 1, 0, 2, 4, 5, 6, 7, 0, 9, 0, 10, 0 };
+
+    // moveZeroesToEndBute(nums);
+    moveZeroesToEndOptimal(nums);
+
+    cout << "Updated Array" << nl;
+    for (int x : nums)
+        cout << x << " ";
+    cout << nl;
+
+    return 0;
+}
