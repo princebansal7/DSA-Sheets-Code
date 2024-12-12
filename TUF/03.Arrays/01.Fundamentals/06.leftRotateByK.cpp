6ee1cc69d6934c697204d4d0fa0bf5e8a172028f
Prince Bansal <75045465+princebansal7@users.noreply.github.com>
2024-12-12 15:20:52 +0530

    TUF+ Array - rotate optimal logic updated

--- TUF/03.Arrays/01.Fundamentals/06.leftRotateByK.cpp
+++ TUF/03.Arrays/01.Fundamentals/06.leftRotateByK.cpp
@@ -7,8 +7,8 @@
 using namespace std;
 #define nl '\n'
 
+// Brute - approach 1
 // TC: O(N*k),  SC: O(1)
-
 void rotateArrayByOne(vector<int>& nums)
 {
     int firstVal = nums[0];
@@ -27,11 +27,63 @@ void rotateArrayByK(vector<int>& nums, int k)
     }
 }
 
+// better - approach 2
+// TC: O(N), SC: O(N)
+void rotateArrayByKBetter(vector<int>& nums, int k)
+{
+    int size = nums.size();
+    k = k % size;
+    vector<int> temp;
+
+    // putting 1st k values in temp array
+    for (int i = 0; i < k; i++) {
+        temp.push_back(nums[i]);
+    }
+
+    // shifting remaining values to left
+    for (int i = k; i < size; i++) {
+        nums[i - k] = nums[i];
+    }
+
+    // putting back (shifted k values in the OG Array)
+    for (int i = 0; i < temp.size(); i++) {
+        nums[(size - k) + i] = temp[i];
+    }
+}
+
+// Optimal - approach 3
+// TC: O(N), SC: O(1)
+
+void reverse(vector<int>& nums, int start, int end)
+{
+    while (start < end) {
+        swap(nums[start++], nums[end--]);
+    }
+}
+
+void rotateArrayByKOptimal(vector<int>& nums, int k)
+{
+    int size = nums.size();
+    k = k % size;
+    // first reverse first k values
+    reverse(nums, 0, k - 1);
+
+    // then reverse remaining values
+    reverse(nums, k, size - 1);
+
+    // lastly reverse the whole array
+    reverse(nums, 0, size - 1);
+}
+
 int main()
 {
     vector<int> nums = { 1, 2, 3, 4, 5 };
-    int k = 5;
-    rotateArrayByK(nums, k);
+    int k = 2;
+
+    // rotateArrayByK(nums, k);
+    // rotateArrayByKBetter(nums, k);
+    rotateArrayByKOptimal(nums, k);
+
     cout << "Array after left rotation by " << k << " place" << nl;
     for (int x : nums)
         cout << x << " ";
